5eba8f769332f32a45ad0fa283ed92587211453d
Ayan <ajan.kohan@gmail.com>
2023-06-06 02:39:33 +0600

    collections task1

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,14 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> res = new ArrayList<>();
+        for(String i : sourceList){
+            int index = sourceList.indexOf(i)+1;
+            if (index %3==0){
+                res.add(i);
+                res.add(i);
+            }
+        }
+        return res;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,22 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int aVal = foo(a);
+        int bVal = foo(b);
+        if (aVal == bVal){
+            return a.compareTo(b);
+        }
+        return Integer.compare(aVal,bVal);
+    }
+    private int foo(String s){
+        int x = Integer.parseInt(s);
+        return 5 * x * x +3;
     }
 }
